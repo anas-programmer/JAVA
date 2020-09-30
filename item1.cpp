18ef6b88aea97923b1937ac2d80cbce880b5f5fe
anas-programmer <68378273+anas-programmer@users.noreply.github.com>
2020-10-01 00:42:51 +0530

    Update item1.cpp

--- item1.cpp
+++ item1.cpp
@@ -1,19 +1,23 @@
 #include <stdio.h>
-int main(){
-int n;
-printf("informe um numero de 4 digitos:\n");
-scanf("%d", &n);
+int main()
+{
+	int n;
+	printf("informe um numero de 4 digitos:\n");
+	scanf("%d", &n);
 
-if((n>=10000) || (n<1000))
-	printf("ERRO, NUMERO INVALIDO\n");	
+	if((n>=10000) || (n<1000))
+		printf("ERRO, NUMERO INVALIDO\n");	
 
-else{		
-int number1(n/1000);
-int number2((n-number1*1000)/100);
-int number3((n-(number1*1000+number2*100))/10);
-int number4(n-(number1*1000+number2*100+number3*10));
-int number5(number4*1000+number3*100+number2*10+number1);
+	else
+	{		
+		int number1(n/1000);
+		int number2((n-number1*1000)/100);
+		int number3((n-(number1*1000+number2*100))/10);
+		int number4(n-(number1*1000+number2*100+number3*10));
+		int number5(number4*1000+number3*100+number2*10+number1);
 
-printf("Resultado = %d\n", number5);}
+		printf("Resultado = %d\n", number5);
+	}
 
-return 0;}
+	return 0;
+}
